1238cec0a719a74524ea23f9e461a15b1c4d5148
sibiriannomad <sibiriannomad@gmail.com>
2024-06-14 12:22:23 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+export const config = {
+    'telegramToken' : '1792482207:AAFkKHZKkc9CigkB4qfJpSR_2VLAum89xEk',
+    'catUrl' : 'https://api.thecatapi.com/v1/images/search',
+    'weatherUrl' : 'http://api.weatherapi.com/v1/current.json?key=32ee203d063147e3bf333209241406&q='
+}
